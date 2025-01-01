ccdcee21499f1ff74d5fbc12b6acd8ca9c5fec0c
stark.shang <stark.shang@outlook.com>
2025-01-01 16:19:30 +0800

    fix: 更新库后修复ledc案例

--- ESP32/basic/ledc/src/main.rs
+++ ESP32/basic/ledc/src/main.rs
@@ -3,62 +3,59 @@
 
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl,
     delay::Delay,
-    gpio::Io,
+    gpio::{Level, Output},
+    init,
     ledc::{channel, timer, LSGlobalClkSource, Ledc, LowSpeed},
-    peripherals::Peripherals,
     prelude::*,
-    system::SystemControl,
 };
 use esp_println::println;
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let peripherals = init({
+        let mut config = esp_hal::Config::default();
+        config.cpu_clock = CpuClock::max();
+        config
+    });
 
     // 配置输出引脚
     // 本例程 Pin12 连接至 LED
-    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let pin12 = io.pins.gpio12;
+    let led = Output::new(peripherals.GPIO12, Level::Low);
     // 配置 LEDC 外设
-    let mut ledc = Ledc::new(peripherals.LEDC, &clocks);
+    let mut ledc = Ledc::new(peripherals.LEDC);
     ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
-    let mut lstimer0 = ledc.get_timer::<LowSpeed>(timer::Number::Timer0);
-    lstimer0.configure(timer::config::Config {
-        duty: timer::config::Duty::Duty5Bit,
-        clock_source: timer::LSClockSource::APBClk,
-        frequency: 24.kHz(),
-    }).unwrap();
+    let mut lstimer0 = ledc.timer::<LowSpeed>(timer::Number::Timer0);
+    lstimer0
+        .configure(timer::config::Config {
+            duty: timer::config::Duty::Duty5Bit,
+            clock_source: timer::LSClockSource::APBClk,
+            frequency: 24.kHz(),
+        })
+        .unwrap();
 
-    let mut channel0 = ledc.get_channel(channel::Number::Channel0, pin12);
-    
-
+    let mut channel0 = ledc.channel(channel::Number::Channel0, led);
 
     let mut brightness = 0;
     let mut increase_or_decrease = true;
-    let delay = Delay::new(&clocks);
+    let delay = Delay::new();
     loop {
         println!("Current Brightness: {brightness}");
-        let _ = channel0.configure(channel::config::Config{
+        let _ = channel0.configure(channel::config::Config {
             timer: &lstimer0,
             duty_pct: brightness,
-            pin_config: channel::config::PinConfig::PushPull
+            pin_config: channel::config::PinConfig::PushPull,
         });
         if brightness > 99 {
             increase_or_decrease = false;
-        }
-        else if brightness < 1 {
+        } else if brightness < 1 {
             increase_or_decrease = true;
             delay.delay_millis(500u32);
         }
 
         if increase_or_decrease {
             brightness += 1;
-        }
-        else {
+        } else {
             brightness -= 1;
         }
 
